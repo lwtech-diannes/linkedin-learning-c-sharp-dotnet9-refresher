09093d30f5d24fccfa66199e1c0b93195bc86afe
lwtech-diannes <diannegermaine@gmail.com>
2025-02-27 16:48:36 -0800

    Operators

--- Start/Overview/Operators/Program.cs
+++ Start/Overview/Operators/Program.cs
@@ -12,22 +12,31 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x / y) * x);
+            Console.WriteLine(a + b);
 
-
-            // TODO: Increment / decrement operators
+            // // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
-
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             // TODO: Operators can be shorthand: a = a + b
+            a += b;
+            Console.WriteLine(a);
 
-
-            // TODO: Logical operators &&, ||
+            // // TODO: Logical operators &&, ||
             Console.WriteLine("----- Logic Operators -----");
-
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
 
             // null-coalescing operators
             string str = null;
+            //str = "hello";
             // TODO: the ?? operator uses left operand if not null, or right one if it is
+            Console.WriteLine(str ?? "Unknown string");
+
 
             
             // TODO: the ??= operator assigns the right operand if the left one is null
@@ -35,6 +44,9 @@ namespace Operators
             // if (variable is null) {
             //    variable = somevalue;
             // }
+
+            str ??= "New String";
+            Console.WriteLine(str);
 
         }
     }
--- Start/Overview/Operators/obj/Debug/net7.0/Operators.AssemblyInfo.cs
+++ Start/Overview/Operators/obj/Debug/net7.0/Operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4f93f85344dd96be37aba975f23a87c0ddaf98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
