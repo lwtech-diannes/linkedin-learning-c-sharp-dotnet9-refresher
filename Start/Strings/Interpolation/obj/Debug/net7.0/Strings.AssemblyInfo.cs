e20b34c4a1127b10e43ef9b8bb50a2641fe6b871
lwtech-diannes <diannegermaine@gmail.com>
2025-02-28 20:03:18 -0800

    String interpolation

--- Start/Strings/Interpolation/obj/Debug/net7.0/Strings.AssemblyInfo.cs
+++ Start/Strings/Interpolation/obj/Debug/net7.0/Strings.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strings")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678060991f613d2b7db382a8a779063203b350ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strings")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strings")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
