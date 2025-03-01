9e7d0606c99cd6093d2de8f8562c53c85aeca2b1
lwtech-diannes <diannegermaine@gmail.com>
2025-02-28 19:31:22 -0800

    String operations

--- Start/Strings/Operations/obj/Debug/net7.0/Operations.AssemblyInfo.cs
+++ Start/Strings/Operations/obj/Debug/net7.0/Operations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ebeb7e1cfa972722ce0712edbc8ee8129e962d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
