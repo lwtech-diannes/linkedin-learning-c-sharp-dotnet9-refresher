104f3c61c8a453466c233451afe970c4729ee2ee
lwtech-diannes <diannegermaine@gmail.com>
2025-02-28 18:57:22 -0800

    While Loops

--- Start/Program Flow/WhileLoops/obj/Debug/net7.0/WhileLoops.AssemblyInfo.cs
+++ Start/Program Flow/WhileLoops/obj/Debug/net7.0/WhileLoops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhileLoops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cbe34bc1675c01f8f04246356756b299451338")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhileLoops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhileLoops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
