94930f1348636a247a995e3b7b71bc8c5806cfbe
lwtech-diannes <diannegermaine@gmail.com>
2025-03-01 13:57:34 -0800

    Functions: Named and default parameters

--- Start/Functions/DefaultParams/obj/Debug/net7.0/Params.AssemblyInfo.cs
+++ Start/Functions/DefaultParams/obj/Debug/net7.0/Params.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Params")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21486ce18287d63be6fb3688e5fb1452e26c5a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Params")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Params")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
