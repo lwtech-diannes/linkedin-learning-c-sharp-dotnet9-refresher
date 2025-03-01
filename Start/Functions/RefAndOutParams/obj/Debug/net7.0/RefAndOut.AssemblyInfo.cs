fb7101d645acd48d2d42617e3dc4623e64d37f5b
lwtech-diannes <diannegermaine@gmail.com>
2025-03-01 14:08:57 -0800

    Functions: Reference and out parameters

--- Start/Functions/RefAndOutParams/obj/Debug/net7.0/RefAndOut.AssemblyInfo.cs
+++ Start/Functions/RefAndOutParams/obj/Debug/net7.0/RefAndOut.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94930f1348636a247a995e3b7b71bc8c5806cfbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefAndOut")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
