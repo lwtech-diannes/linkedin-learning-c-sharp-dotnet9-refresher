a61c08cddc24edffd6be2b3fc7df6606cfe5d9b0
lwtech-diannes <diannegermaine@gmail.com>
2025-03-01 17:05:59 -0800

    Defining C# Classes

--- Start/Classes/Defining/Book.cs
+++ Start/Classes/Defining/Book.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 
 namespace Defining
 {
@@ -6,12 +7,20 @@ namespace Defining
     public class Book
     {
         // TODO: classes have member variables, or "fields" to hold data
-
-
+        string _name;
+        string _author;
+        int _pagecount;
+    
         // TODO: classes have one or more constructors
-
+        public Book(string name, string author, int pagecount){
+            _name = name;
+            _author = author;
+            _pagecount = pagecount;
+        }
 
         // TODO: methods are used to operate on the class and data
-
+        public string GetDescription(){
+            return $"{_name} by {_author}";
+        }
     }
 }
